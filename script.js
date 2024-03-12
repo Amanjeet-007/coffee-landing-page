4021dac2e029d5a97a8904ee163c10b72a9c7000
Amanjeet-007 <146502426+Amanjeet-007@users.noreply.github.com>
2024-03-12 08:33:49 +0530

    some interactive changes

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let sear = document.getElementById("sear");
 let elm = document.querySelector(".sea");
 let a = 0;
-
+let d = document.querySelector("main");
+let gh = d.addEventListener("click",function(){
+    elm.style.display = "none";
+    a = 0;
+})
 sear.addEventListener("click",function(){
     if(a==0){
         elm.style.display = "flex";
         a = 1;
     }
     else{
+        gh()
         elm.style.display = "none";
         a = 0;
     }
