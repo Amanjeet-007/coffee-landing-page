307fbf322cfd67ac192ef759c979f8463047a597
Amanjeet-007 <146502426+Amanjeet-007@users.noreply.github.com>
2024-03-21 09:28:11 +0530

    changed

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ let sear = document.getElementById("sear");
 let elm = document.querySelector(".sea");
 let a = 0;
 let d = document.querySelector("main");
-let gh = d.addEventListener("click",function(){
+let gh = d.addEventListener("click", function () {
     elm.style.display = "none";
     a = 0;
 })
-sear.addEventListener("click",function(){
-    if(a==0){
+sear.addEventListener("click", function () {
+    if (a == 0) {
         elm.style.display = "flex";
         a = 1;
     }
-    else{
+    else {
         gh()
         elm.style.display = "none";
         a = 0;
     }
-    
-   
+
+
 })
+
