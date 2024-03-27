920a0989afdfd43eaf020c76deb0b0feb19fe3fc
Amanjeet-007 <146502426+Amanjeet-007@users.noreply.github.com>
2024-03-27 16:29:45 +0530

    almost done

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ let sear = document.getElementById("sear");
 let elm = document.querySelector(".sea");
 let a = 0;
 let d = document.querySelector("main");
-let gh = d.addEventListener("click", function () {
-    elm.style.display = "none";
-    a = 0;
-})
+let cut = document.querySelector(".cut");
+
+
+
+// let gh = d.addEventListener("click", function () {
+//     elm.style.display = "none";
+//     a = 0;
+// })
 sear.addEventListener("click", function () {
     if (a == 0) {
         elm.style.display = "flex";
@@ -18,5 +22,25 @@ sear.addEventListener("click", function () {
     }
 
 
+})
+cut.addEventListener("click",()=>{
+    elm.style.display = "none";
+    a=0
+})
+// like
+let heart = document.querySelectorAll(".heart");
+heart.forEach((el)=>{
+    el.addEventListener("click",()=>{
+        if(a==0){
+            el.style.color = "red";
+            a = 1 ;
+        }
+        else{
+            el.style.color = "unset";
+             a=0;
+        }
+        
+    })
+    
 })
 
