9314823ddc6348cedeea8bb891d9e5302ab946be
Amanjeet-007 <146502426+Amanjeet-007@users.noreply.github.com>
2024-04-03 19:36:25 +0530

    some functionality are added

--- script.js
+++ script.js
@@ -2,33 +2,49 @@ let sear = document.getElementById("sear");
 let elm = document.querySelector(".sea");
 let a = 0;
 let d = document.querySelector("main");
-let cut = document.querySelector(".cut");
+let cut = document.querySelectorAll(".cut");
+let icon = document.querySelector("#shop");
+let box =  document.querySelector(".cartbox");
+let heart = document.querySelectorAll(".heart");
+let count = document.querySelector(".count");
+let num = 0;
+function add(a){ 
+    num = ++num
+    count.textContent = `${num}`;
+    // icon.textContent = `${num} Item added in cart.`
+    mess
+}
 
 
 
-// let gh = d.addEventListener("click", function () {
-//     elm.style.display = "none";
-//     a = 0;
-// })
+
+
+//search functionality
 sear.addEventListener("click", function () {
+    let a = 0;
     if (a == 0) {
         elm.style.display = "flex";
+        elm.style.visibility = "visible";
         a = 1;
     }
     else {
-        gh()
-        elm.style.display = "none";
+        elm.style.visibility = "hidden";
         a = 0;
     }
 
 
 })
-cut.addEventListener("click",()=>{
-    elm.style.display = "none";
-    a=0
+function iconC(){
+   box.style.visibility = "visible";
+}
+// X cut functionality
+cut.forEach((el)=>{
+    el.addEventListener("click",()=>{
+    el.parentElement.style.visibility = "hidden";
+    })
+    
 })
 // like
-let heart = document.querySelectorAll(".heart");
 heart.forEach((el)=>{
     el.addEventListener("click",()=>{
         if(a==0){
